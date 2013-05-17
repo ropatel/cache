8fa36e616a8ca8080f763ed34765016cd5388221
Rohin Patel <ropatel@gmail.com>
2013-05-17 14:32:48 -0700

    Update constructor error message.

--- LRUCache/src/com/rohinp/mentoring/cache/MemoryManagerConfig.java
+++ LRUCache/src/com/rohinp/mentoring/cache/MemoryManagerConfig.java
@@ -5,7 +5,11 @@ public class MemoryManagerConfig {
 	private int maxMemory_ = 0;
 	private int blockSize_ = 0;
 	
+	// This value was arbitrarily chosen.  This value should likely be 
+	// computed based upon the host specifications.
 	private static final int DEFAULT_MAX_SIZE = 6000;
+	
+	// This value was chosen <need an explanation here>
 	private static final int BLOCK_SIZE = 4;
 	
 	public MemoryManagerConfig()
@@ -16,7 +20,7 @@ public class MemoryManagerConfig {
 	public MemoryManagerConfig(final int maxMemory,final int blockSize) throws IllegalArgumentException
 	{
 		if (maxMemory % blockSize > 0) {
-			throw new IllegalArgumentException("Maximum memory size must be evenly divisible by the block size.");
+			throw new IllegalArgumentException("Maximum memory size must be a multiple the block size.");
 		} else {
 			maxMemory_ = maxMemory;
 			blockSize_ = blockSize;
