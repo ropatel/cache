02f9976e7db3852516a1fce3c3910e24ae54e7c2
rohinpatel <rohinpatel@192.168.1.3>
2013-05-30 21:43:36 -0700

    Update ByteArray class to wrap functionality around the byte array.

--- LRUCache/src/com/rohinp/mentoring/cache/ByteArray.java
+++ LRUCache/src/com/rohinp/mentoring/cache/ByteArray.java
@@ -1,7 +1,32 @@
 package com.rohinp.mentoring.cache;
 
-public class ByteArray {
+import java.util.Iterator;
+
+public class ByteArray implements Iterator<Byte>{
 	
-	public byte[] value;
+	private final byte[] value_;
+	private int index_;
+	private final int size_;
+	
+	public ByteArray(byte[] value) {
+		value_ = value;
+		index_ = 0;
+		size_ = value.length;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return index_ <= (size_ - 1);
+	}
+
+	@Override
+	public Byte next() {
+		return value_[index_++];
+		// TODO Should throw an exception if you attempt to call out of bounds.
+	}
+
+	@Override
+	public void remove() {		
+	}
 
 }
