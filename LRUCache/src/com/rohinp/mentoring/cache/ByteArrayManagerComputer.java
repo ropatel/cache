7cda845109d011dad0bb1e3981ebfc9282c1a5ca
rohinpatel <rohinpatel@192.168.1.3>
2013-05-30 21:44:19 -0700

    Add ByteArrayManagerComputer to centralize compute business logic.

---
+++ LRUCache/src/com/rohinp/mentoring/cache/ByteArrayManagerComputer.java
@@ -0,0 +1,13 @@
+package com.rohinp.mentoring.cache;
+
+public class ByteArrayManagerComputer {
+	
+	private final MemoryManagerConfig config_;
+	
+	public ByteArrayManagerComputer(MemoryManagerConfig config) {
+		config_ = config;
+	}
+	
+	
+
+}
